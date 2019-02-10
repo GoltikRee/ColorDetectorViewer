bb96082f31194296317078faf4c50975389f7f5c
Ilya <serverprom@yandex.ru>
2019-02-10 17:57:02 +0300

    initial commit

---
+++ ColorDetectorViewer/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+using MessageBox = System.Windows.MessageBox;
+
+namespace ColorDetectorViewer
+{
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        SerialPort _currentPort = null;
+        private delegate void UpdateDelegate(byte[] values);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateColor()
+        {
+            while (_currentPort.IsOpen)
+            {
+                if (!_currentPort.IsOpen) return;
+                try 
+                {
+                    //_currentPort.DiscardInBuffer();
+                    string strFromPort = _currentPort.ReadLine();
+                    var values = strFromPort
+                        .Replace("$#$", "")
+                        .Replace("@!@", "")
+                        .Replace("\r", "")
+                        .Split(',')
+                        .Select(x => Convert.ToByte(Convert.ToInt32(x))).ToArray();
+
+                    ColorCanvas.Dispatcher.BeginInvoke(new UpdateDelegate(UpdateCanvas), values);
+                }
+                catch (Exception exception)
+                {
+                    throw exception; //TODO: Implement exception handling
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _currentPort = new SerialPort("COM5", 9600);
+            _currentPort.Open();
+            Thread receiverThread = new Thread(UpdateColor);
+            receiverThread.Start();
+        }
+
+        private void UpdateCanvas(byte[] values)
+        {
+            ColorCanvas.R = values[0];
+            ColorCanvas.G = values[1];
+            ColorCanvas.B = values[2];
+        }
+    }
+}
---
+++ ColorDetectorViewer/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorDetectorViewer
+{
+    public static class Utils
+    {
+        public static bool IsArduinoDetected(SerialPort currentPort)
+        {
+            try
+            {
+                if (currentPort.PortName == "COM1")
+                    return false;
+                currentPort.Open();
+                System.Threading.Thread.Sleep(1000);
+                // небольшая пауза, ведь SerialPort не терпит суеты
+
+                string returnMessage = currentPort.ReadLine();
+                currentPort.Close();
+
+                // необходимо чтобы void loop() в скетче содержал код Serial.println("Info from Arduino");
+                return returnMessage.Contains("$#$");
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
